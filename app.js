7b8fac107db5fa96233472e0a22ec1e47415969f
Þorgeir <thorgeir93@gmail.com>
2020-04-21 18:54:38 +0000

    start server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
+});
