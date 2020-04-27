b7d17427538f284782c056887cf2d2841338f325
Þorgeir <thorgeir93@gmail.com>
2020-04-27 20:59:07 +0000

    minor fix

--- public/scripts.js
+++ public/scripts.js
@@ -7,10 +7,10 @@ inputs.forEach(input => {
 
         if (ans === userAns) {
             input.style.backgroundColor = "#bdffe2";
-            input.style.border = "3px solid #30e395";
+            input.style.border = "2px solid #30e395";
         } else {
             input.style.backgroundColor = "#ffb3b3";
-            input.style.border = "3px solid #e84545";
+            input.style.border = "2px solid #e84545";
         }
     });
 });
